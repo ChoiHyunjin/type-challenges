90bc2737eb95d11f0f7209e355071b1ef6c04e64
choihyunjin <guswls3373@gmail.com>
2023-07-12 16:56:06 +0900

    imp(medium): implement remove index signature

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,4 @@
-type RemoveIndexSignature<T> = any
+// todo: 다시 풀기
+type RemoveIndexSignature<T, P = PropertyKey> = {
+  [key in keyof T as P extends key ? never : key extends P ? key : never]: T[key]
+}
