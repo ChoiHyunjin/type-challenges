d6bf45948499fc92482c2cd2853916264c24f9b7
choihyunjin <guswls3373@gmail.com>
2023-07-18 09:55:13 +0900

    imp(medium): imp PartialByKeys

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,9 @@
-type PartialByKeys<T, K> = any
+type PartialByKeys<T extends object, K = keyof T> = Combine<{
+  [key in keyof T as key extends K ? key : never]?: T[key]
+} & {
+  [key in keyof T as key extends K ? never : key]: T[key]
+}>
+
+type Combine<T extends object> = {
+  [key in keyof T]: T[key]
+}
