22ca17d6112f6edf94601a35ae04148aca584ae4
choihyunjin <guswls3373@gmail.com>
2023-07-20 10:41:52 +0900

    imp(medium): imp some challenges

--- questions/02946-medium-objectentries/template.ts
+++ questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,9 @@
-type ObjectEntries<T> = any
+// todo: 재도전
+// type ObjectEntries<T, K = keyof Required<T>> = K extends keyof Required<T>
+//   ? [K, RemoveUndefined<T[K]>] : never
+//
+// type RemoveUndefined<T> = [T] extends [undefined] ? T : Exclude<T, undefined>
+
+type ObjectEntries<T, U = Required<T>> = {
+  [K in keyof U]: [K, U[K] extends never ? undefined : U[K]]
+}[keyof U]
--- questions/02946-medium-objectentries/test-cases.ts
+++ questions/02946-medium-objectentries/test-cases.ts
@@ -6,6 +6,13 @@ interface Model {
   locations: string[] | null
 }
 
+type Test1 = ObjectEntries<{ key?: undefined }>
+type Test11 = ObjectEntries<{ key: undefined }>
+type Test2 = ObjectEntries<Partial<Model>>
+
+type Test3 = keyof Required<Partial<Model>>
+type Test4 = Required<Partial<Model>>
+
 type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null]
 
 type cases = [
