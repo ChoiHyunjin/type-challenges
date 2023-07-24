81cd3c2ff0f65271fec562ed7f23b3689b714fbc
choihyunjin <guswls3373@gmail.com>
2023-07-24 10:19:40 +0900

    imp(medium): imp TupleToNestedObject

--- questions/03188-medium-tuple-to-nested-object/template.ts
+++ questions/03188-medium-tuple-to-nested-object/template.ts
@@ -1,1 +1,8 @@
-type TupleToNestedObject<T, U> = any
+type TupleToNestedObject<T extends string[], U> =
+    T extends [infer F, ...infer R]
+      ? F extends string ?
+        R extends string[] ?
+            { [key in F]: TupleToNestedObject<R, U> }
+          : U
+        : U
+      : U
