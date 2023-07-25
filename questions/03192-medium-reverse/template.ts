a7b4e613f5395475ff8cd7479c45d937eabd7570
choihyunjin <guswls3373@gmail.com>
2023-07-25 10:31:55 +0900

    imp(medium): imp problems

--- questions/03192-medium-reverse/template.ts
+++ questions/03192-medium-reverse/template.ts
@@ -1,1 +1,3 @@
-type Reverse<T> = any
+type Reverse<T extends string[]> = T extends [infer A, ...infer B]
+  ? B extends any[] ? [...Reverse<B>, A] : T
+  : T
--- questions/03196-medium-flip-arguments/template.ts
+++ questions/03196-medium-flip-arguments/template.ts
@@ -1,1 +1,6 @@
-type FlipArguments<T> = any
+type FlipArguments<T extends (...arg: any[]) => any>
+    = T extends ((...r: infer R) => infer K)
+      ? R extends any[]
+        ? ((...arg: Reverse<R>) => K)
+        : T
+      : T
