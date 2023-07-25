27b5602965e28393e67a27ad734161f4caef132b
choihyunjin <guswls3373@gmail.com>
2023-07-26 08:45:22 +0900

    imp(medium): imp FlattenDepth

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -1,1 +1,10 @@
-type FlattenDepth = any
+type MinusOne2<T extends number> = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9][T]
+
+type FlattenDepth<T extends any[], N extends number = 1> = N extends 0
+  ? T
+  : T extends [infer F, ...infer R]
+    ? [...(F extends any[]
+        ? FlattenDepth<F, MinusOne2<N>> : [F]),
+      ...(R extends any[] ? FlattenDepth<R, N> : [R])]
+    : T
+
