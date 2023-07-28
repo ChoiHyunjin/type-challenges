32b527b7ffe9b2f10fbd9a7b92879883bb870e23
choihyunjin <guswls3373@gmail.com>
2023-07-28 09:51:57 +0900

    imp(medium): imp BEM

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,6 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type GUS<E extends string[]> = E extends [infer F, ...infer R]
+  ? R extends string[] ? F | GUS<R> : F
+  : never
+
+type BEM<B extends string, E extends string[], M extends string[]> =
+    `${B}${GUS<E> extends never ? '' : GUS<E> extends string ? `__${GUS<E>}` : ''}${GUS<M> extends never ? '' : GUS<M> extends string ? `--${GUS<M>}` : ''}`
