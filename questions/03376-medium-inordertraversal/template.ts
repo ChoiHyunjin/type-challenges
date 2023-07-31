3fdbda2a136324b12489ebb88c391c0f716873f9
choihyunjin <guswls3373@gmail.com>
2023-07-31 10:38:11 +0900

    refactor(medium): InorderTraversal

--- questions/03376-medium-inordertraversal/template.ts
+++ questions/03376-medium-inordertraversal/template.ts
@@ -9,3 +9,9 @@ type InorderTraversal<T extends TreeNode | null> = T extends TreeNode
       T['val'],
       ...(T['right'] extends TreeNode ? InorderTraversal<T['right']> : [])]
   : []
+
+// todo: 더 다듬기
+// 깔끔 답안
+// type InorderTraversal<T extends TreeNode | null, NT extends TreeNode = NonNullable<T> > = T extends null
+//   ? []
+//   : [...InorderTraversal<NT['left']>, NT['val'], ...InorderTraversal<NT['right']>]
