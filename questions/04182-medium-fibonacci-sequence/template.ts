c87c7e819cf71ea878a582f4609836bc922cd206
choihyunjin <guswls3373@gmail.com>
2023-08-02 10:07:53 +0900

    feat(medium): imp Fibonacci

--- questions/04182-medium-fibonacci-sequence/template.ts
+++ questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,5 @@
-type Fibonacci<T extends number> = any
+//  todo: 다시 풀기
+type Fibonacci<T extends number, Array extends number[] = [1], Prev extends number[] = [], Count extends number[] = [1]> =
+    Count['length'] extends T
+      ? Array['length']
+      : Fibonacci<T, [...Array, ...Prev], Array, [...Count, 1]>
