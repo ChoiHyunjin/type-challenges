cc22e83020cb0c47c6cde16c10b6639f33010018
choihyunjin <guswls3373@gmail.com>
2023-08-03 09:55:36 +0900

    feat(medium): imp AllCombinations

--- questions/04260-medium-nomiwase/template.ts
+++ questions/04260-medium-nomiwase/template.ts
@@ -1,1 +1,7 @@
-type AllCombinations<S> = any
+type AllCombinations<S extends string> = S extends `${infer F}${infer R}`
+  ? AddCharacter<AllCombinations<R>, F>
+  : '' | S
+
+type AddCharacter<S extends string, T extends string> = S extends `${infer F}${infer R}`
+  ? `${F}${AddCharacter<R, T>}` | `${AddCharacter<R, T>}${F}`
+  : '' | T | S
