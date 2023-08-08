580faf9bdc2cf2cb4ec86ff3691ed500f8c0d33d
choihyunjin <guswls3373@gmail.com>
2023-08-08 10:29:47 +0900

    feat(medium): imp zip

--- questions/04471-medium-zip/template.ts
+++ questions/04471-medium-zip/template.ts
@@ -1,1 +1,16 @@
-type Zip<T, U> = any
+type Zip<T extends any[], U extends any[]> =
+    [T, U] extends [[infer T1, ...infer RestT], [infer U1, ...infer RestU]]
+      ? [[T1, U1], ...Zip<RestT, RestU>]
+      : []
+
+// type Zip<T extends any[], U extends any[]> = T extends [unknown, ...infer TR]
+//     ? U extends [unknown, ...infer UR]
+//         ? [[T[0], U[0]], ...Zip<TR, UR>]
+//         : []
+//     : []
+
+// type Zip<T extends any[], U extends any[], V extends any[] = []> =
+//     V['length'] extends T['length'] | U['length']
+//       ? V
+//       : Zip<T, U, [...V, [T[V['length']], U[V['length']]]]>
+
