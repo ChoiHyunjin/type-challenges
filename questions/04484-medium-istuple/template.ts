48b69586f45ed45d64e16eb2358ea54d670fa633
choihyunjin <guswls3373@gmail.com>
2023-08-08 10:29:42 +0900

    feat(medium): imp isTuple

--- questions/04484-medium-istuple/template.ts
+++ questions/04484-medium-istuple/template.ts
@@ -1,1 +1,17 @@
-type IsTuple<T> = any
+// todo: 다시 풀기
+//  내가 쓴 답
+// type IsTuple<T> = [T] extends [never]
+//   ? false
+//   : T extends []
+//     ? true
+//     : T extends readonly [unknown]
+//       ? true
+//       : false
+
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends readonly any[]
+    ? number extends T['length']
+      ? false
+      : true
+    : false
