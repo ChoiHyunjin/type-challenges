df97af92683f3bdf95943d6f94b51c1f07a9ac80
choihyunjin <guswls3373@gmail.com>
2023-08-09 12:23:25 +0900

    feat(medium): imp Chunk

--- questions/04499-medium-chunk/template.ts
+++ questions/04499-medium-chunk/template.ts
@@ -1,1 +1,6 @@
-type Chunk = any
+// todo: 다시 풀기
+type Chunk<T extends any[], N extends number, V extends any[] = []> =
+    V['length'] extends N ? [V, ...Chunk<T, N>]
+      : T extends [infer T1, ...infer RestT]
+        ? Chunk<RestT, N, [...V, T1]>
+        : V extends [] ? V : [V]
