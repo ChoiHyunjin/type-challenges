dfedfacd58c8cc37e14620d4ac9f3731aaf1937c
choihyunjin <guswls3373@gmail.com>
2023-08-14 09:58:08 +0900

    feat(medium): imp fill

--- questions/04518-medium-fill/template.ts
+++ questions/04518-medium-fill/template.ts
@@ -1,6 +1,14 @@
 type Fill<
-  T extends unknown[],
-  N,
-  Start extends number = 0,
-  End extends number = T['length'],
-> = any
+    T extends unknown[],
+    N,
+    Start extends number = 0,
+    End extends number = T['length'],
+    Pointer extends unknown[] = [],
+    Started extends boolean = Pointer['length'] extends Start ? true : false,
+> = Pointer['length'] extends End
+  ? T
+  : T extends [infer T1, ...infer Rest]
+    ? Started extends true
+      ? [N, ...Fill<Rest, N, Start, End, [...Pointer, 0], Started>]
+      : [T1, ...Fill<Rest, N, Start, End, [...Pointer, 0]>]
+    : T
