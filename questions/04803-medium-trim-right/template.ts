bccc672c3f6d4d5f5af015fc2c683f8e97706f9d
choihyunjin <guswls3373@gmail.com>
2023-08-14 09:58:16 +0900

    feat(medium): imp trim

--- questions/04803-medium-trim-right/template.ts
+++ questions/04803-medium-trim-right/template.ts
@@ -1,1 +1,6 @@
-type TrimRight<S extends string> = any
+type TrimRight<S extends string, Temp extends string = ''> =
+    S extends `${infer C}${infer Rest}`
+      ? C extends ' ' | '\n' | '\t'
+        ? TrimRight<Rest, `${Temp}${C}`>
+        : `${Temp}${C}${TrimRight<Rest>}`
+      : S
