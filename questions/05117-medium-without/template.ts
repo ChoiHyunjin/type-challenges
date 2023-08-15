9b12d11a0acb7c5965da780bd5800089fc5bf753
choihyunjin <guswls3373@gmail.com>
2023-08-16 08:58:42 +0900

    feat(medium): imp Without

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,5 @@
-type Without<T, U> = any
+type Without<T, U> = T extends [infer T1, ...infer Rest]
+  ? U extends any[]
+    ? T1 extends U[number] ? Without<Rest, U> : [T1, ...Without<Rest, U>]
+    : T1 extends U ? Without<Rest, U> : [T1, ...Without<Rest, U>]
+  : T
