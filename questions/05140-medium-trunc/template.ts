c479f183fd956ed7ce47327689dc38e9dc7b29e1
choihyunjin <guswls3373@gmail.com>
2023-08-17 09:43:39 +0900

    feat(medium): imp trunc

--- questions/05140-medium-trunc/template.ts
+++ questions/05140-medium-trunc/template.ts
@@ -1,1 +1,6 @@
-type Trunc = any
+type Trunc<T extends number | string> =
+    `${T}` extends `${infer F}${infer Rest}`
+      ? F extends '.'
+        ? ''
+        : `${F}${Trunc<Rest>}`
+      : T
