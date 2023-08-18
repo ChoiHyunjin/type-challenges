323e8f88e28196f31e578ba1b6c72ad6b5918eb8
choihyunjin <guswls3373@gmail.com>
2023-08-18 09:58:33 +0900

    feat(medium): imp IndexOf

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,13 @@
-type IndexOf<T, U> = any
+type IndexOf<T, U, V extends number[] = []> = T extends [infer T1, ...infer Rest]
+  ? (<W>() => W extends T1 ? 0 : 1) extends (<W>() => W extends U ? 0 : 1)
+      ? V['length']
+      : IndexOf<Rest, U, [...V, 0]>
+  : -1
+
+// type IndexOf<T, U, V extends number[] = []> = T extends [infer T1, ...infer Rest]
+//   ? [T1, U] extends [U, T1] ? V['length'] : IndexOf<Rest, U, [...V, 0]>
+//   : -1
+
+// type IndexOf<T, U, V extends number[] = []> = T extends [infer T1, ...infer Rest]
+//     ? T1 extends U ? V['length'] : IndexOf<Rest, U, [...V, 0]>
+//     : -1
