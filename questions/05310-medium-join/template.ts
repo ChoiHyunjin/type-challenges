63ab53c9264c4f73b3d5282192be3d4c91faa545
choihyunjin <guswls3373@gmail.com>
2023-08-21 10:32:46 +0900

    feat(medium): imp Join

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,6 @@
-type Join<T, U> = any
+type Join<T extends unknown[], U extends string | number> = T extends [infer T1 extends string, ...infer Rest]
+  ? Rest['length'] extends 0
+    ? T1
+    : `${T1}${U}${Join<Rest, U>}`
+  : ''
+
