6b643f953edf228e4720725bd947c2b7cc4897bb
choihyunjin <guswls3373@gmail.com>
2023-08-22 12:13:27 +0900

    feat(medium): imp Unique

--- questions/05360-medium-unique/template.ts
+++ questions/05360-medium-unique/template.ts
@@ -1,1 +1,9 @@
-type Unique<T> = any
+type Unique<T, U extends any[] = []> = T extends [infer T1, ...infer Rest]
+  ? Unique<Rest, Has<U, T1> extends true ? U : [...U, T1]>
+  : U
+
+type Has<T extends any[], U> = T extends [infer T1, ...infer Rest]
+  ? (<W>() => W extends T1 ? 0 : 1) extends(<W>() => W extends U ? 0 : 1)
+      ? true
+      : Has<Rest, U>
+  : false
