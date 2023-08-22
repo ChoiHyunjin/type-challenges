93599248e11b4d5add24853b163b9b6b24c6ad84
choihyunjin <guswls3373@gmail.com>
2023-08-22 09:55:20 +0900

    feat(medium): imp LastIndexOf

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,8 @@
-type LastIndexOf<T, U> = any
+type IsEqual<T, U> = U extends T ? T extends U ? true : false : false
+
+type LastIndexOf<T, U>
+    = T extends [...infer Rest, infer TL]
+      ? IsEqual<TL, U> extends true
+        ? Rest['length']
+        : LastIndexOf<Rest, U>
+      : -1
