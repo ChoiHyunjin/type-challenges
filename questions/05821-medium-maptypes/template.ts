f1350d07097b9dac9534fd79c907a4124a723007
choihyunjin <guswls3373@gmail.com>
2023-08-24 09:51:13 +0900

    feat(medium): imp MapTypes

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,12 @@
-type MapTypes<T, R> = any
+// todo: 다시 풀기
+type MapTypes<T extends object, R extends { mapFrom: any; mapTo: any }> = {
+  [key in keyof T]: T[key] extends R['mapFrom']
+    ? R extends { mapFrom: T[key] }
+      ? R['mapTo']
+      : never
+    : T[key]
+}
+
+// type MapTypes<T extends object, R extends { mapFrom: any; mapTo: any }> = {
+//   [key in keyof T]: T[key] extends R['mapFrom'] ? R['mapTo'] : T[key]
+// }
