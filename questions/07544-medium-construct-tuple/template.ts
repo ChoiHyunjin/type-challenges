9f74edb0dca1a475398da8e74df7c111ac119d49
choihyunjin <guswls3373@gmail.com>
2023-08-25 09:41:34 +0900

    feat(medium): imp ConstructTuple

--- questions/07544-medium-construct-tuple/template.ts
+++ questions/07544-medium-construct-tuple/template.ts
@@ -1,1 +1,16 @@
-type ConstructTuple<L extends number> = any
+type ConstructTuple<L extends number, Answer extends unknown[] = [], S extends string = `${L}`> =
+    S extends `${infer S1 extends string}${infer Rest}`
+      ? ConstructTuple<L, AddNTime<S1, Answer>, Rest>
+      : Answer
+
+type AddNTime<N extends string, A extends unknown[] = [], Counter extends any[] = []> =
+    `${Counter['length']}` extends N
+      ? Multiple10<A>
+      : [...AddNTime<N, A, [...Counter, 0]>, unknown]
+
+type Multiple10<N extends unknown[], Count extends any[] = []> =
+    Count['length'] extends 9 ? N : [...Multiple10<N, [...Count, 0]>, ...N]
+
+// 1차
+// type ConstructTuple<L extends number, Answer extends unknown[] = []> =
+//  Answer['length'] extends L ? Answer : ConstructTuple<L, [...Answer, unknown]>
