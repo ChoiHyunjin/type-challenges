469b4a029cd8c19e839cd639959f4ac3768cf1e5
choihyunjin <guswls3373@gmail.com>
2023-08-28 10:00:25 +0900

    feat(medium): imp NumberRange

--- questions/08640-medium-number-range/template.ts
+++ questions/08640-medium-number-range/template.ts
@@ -1,1 +1,7 @@
-type NumberRange<L, H> = any
+type NumberRange<L extends number, H extends number> =
+    Exclude<UnionUntil<H>, UnionUntil<L>> | L
+
+type UnionUntil<L extends number, Result extends number = L, Count extends unknown[] = []> =
+    Count['length'] extends L
+      ? Result
+      : UnionUntil<L, Result | Count['length'], [...Count, unknown]>
