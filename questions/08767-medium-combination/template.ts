12cbf0a69f9b62bdcee01070d9749fc565faf888
choihyunjin <guswls3373@gmail.com>
2023-08-30 10:33:33 +0900

    feat(medium): imp Subsequence

--- questions/08767-medium-combination/template.ts
+++ questions/08767-medium-combination/template.ts
@@ -1,1 +1,4 @@
-type Combination<T extends string[]> = any
+// todo: 다시 풀기
+type Combination<T extends string[], All = T[number], Item = All> = Item extends string
+  ? Item | `${Item} ${Combination<[], Exclude<All, Item>>}`
+  : never
--- questions/08987-medium-subsequence/template.ts
+++ questions/08987-medium-subsequence/template.ts
@@ -1,1 +1,7 @@
-type Subsequence<T extends any[]> = any
+type Subsequence<T extends any[]> = T extends [infer T1, ...infer Rest]
+  ? AddValue<Subsequence<Rest>, T1>
+  : []
+
+type AddValue<T extends any[], V> = T extends T
+  ? T | [V, ...T]
+  : never
