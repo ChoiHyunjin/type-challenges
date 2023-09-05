c7291bce423314dccff68c4398c35a27a84af0c0
choihyunjin <guswls3373@gmail.com>
2023-09-05 12:16:03 +0900

    feat(medium): imp Chainable

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,10 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T = {}> = {
+  option<U extends string, V>(
+    key: U extends keyof T
+      ? V extends T[U]
+        ? never
+        : U
+      : U,
+    value: V): Chainable<Omit<T, U> & Record<U, V>>
+  get(): T
 }
