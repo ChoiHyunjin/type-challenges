c8c47d7c0065e74ea1dfc3d0bb5d0fa40f5c24ff
choihyunjin <guswls3373@gmail.com>
2023-09-06 12:12:22 +0900

    feat(medium): imp CheckRepeatedChars

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,28 @@
-type CheckRepeatedChars<T extends string> = any
+// type CheckRepeatedChars<T extends string> = T extends `${infer T1}${infer Rest}`
+//   ? HasChar<Rest, T1> extends true
+//     ? true
+//     : CheckRepeatedChars<Rest>
+//   : false
+//
+// type HasChar<T extends string, C extends string> =
+//     T extends `${infer T1}${infer Rest}`
+//       ? T1 extends C ? true : HasChar<Rest, C>
+//       : false
+
+type CheckRepeatedChars<T extends string> = T extends `${infer T1}${infer Rest}`
+  ? Rest extends `${string}${T1}${string}`
+    ? true
+    : CheckRepeatedChars<Rest>
+  : false
+
+// type CheckRepeatedChars<T extends string, U extends string[] = []> =
+//     T extends `${infer T1}${infer Rest}`
+//       ? HasChar<T1, U> extends true ? true : CheckRepeatedChars<Rest, [...U, T1]>
+//       : false
+//
+// type HasChar<T extends string, U extends string[]> =
+//     U extends [infer U1, ...infer Rest extends string[]]
+//       ? T extends U1
+//         ? true
+//         : HasChar<T, Rest>
+//       : false
