eb0263cd9c1e493e542b1d251a1f989e314dec96
choihyunjin <guswls3373@gmail.com>
2023-09-08 09:37:36 +0900

    feat(medium): imp FirstUniqueCharIndex

--- questions/09286-medium-firstuniquecharindex/template.ts
+++ questions/09286-medium-firstuniquecharindex/template.ts
@@ -1,1 +1,8 @@
-type FirstUniqueCharIndex<T extends string> = any
+type FirstUniqueCharIndex<T extends string, Pointer extends string[] = []> =
+    T extends `${infer T1}${infer Rest}`
+      ? Rest extends `${string}${T1}${string}`
+        ? FirstUniqueCharIndex<Rest, [...Pointer, T1]>
+        : T1 extends Pointer[number]
+          ? FirstUniqueCharIndex<Rest, [...Pointer, T1]>
+          : Pointer['length']
+      : -1
