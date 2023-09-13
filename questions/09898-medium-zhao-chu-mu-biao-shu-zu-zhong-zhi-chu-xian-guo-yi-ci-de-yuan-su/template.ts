df2ed4a649f7475d7b9f4090fb53cab6410426d6
choihyunjin <guswls3373@gmail.com>
2023-09-13 10:17:48 +0900

    feat(medium): imp FindEles

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,11 @@
-type FindEles<T extends any[]> = any
+type RemoveDuplicate<T extends any[], U> = T extends [infer T1, ...infer Rest]
+  ? T1 extends U ? Rest : [T1, ...RemoveDuplicate<Rest, U>]
+  : T
+
+type FindEles<T extends any[], Result extends any[] = [], Duplicated = never> =
+    T extends [infer T1, ...infer Rest]
+      ? T1 extends Duplicated
+        ? FindEles<Rest, RemoveDuplicate<Result, T1>, Duplicated>
+        : FindEles<Rest, [...Result, T1], Duplicated | T1>
+      : Result
+
