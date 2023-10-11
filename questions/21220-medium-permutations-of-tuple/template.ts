0b5a7bfb275359525dfca004c6a900457f49bc0b
choihyunjin <guswls3373@gmail.com>
2023-10-11 12:07:38 +0900

    feat(medium): imp PermutationsOfTuple

--- questions/21220-medium-permutations-of-tuple/template.ts
+++ questions/21220-medium-permutations-of-tuple/template.ts
@@ -1,1 +1,13 @@
-type PermutationsOfTuple<T extends unknown[]> = any
+type PermutationsOfTuple<T extends unknown[], R extends unknown[] = []> = T extends [infer T1, ...infer Rest]
+  ? PermutationsOfTuple<Rest, Permutations<T1, R>>
+  : R
+
+type Permutations<T, A extends unknown[], P extends unknown[] = []> =
+    A['length'] extends P['length']
+      ? [...A, T]
+      : PutAt<T, A, P['length']> | Permutations<T, A, [...P, unknown]>
+
+type PutAt<T, A extends unknown[], P extends number, Prev extends unknown[] = []> =
+    P extends Prev['length'] ? [...Prev, T, ...A] : A extends [infer A1, ...infer Rest]
+      ? PutAt<T, Rest, P, [...Prev, A1]>
+      : never
