46fee8be5ab373566c80eeea0152e7e93e8a3612
choihyunjin <guswls3373@gmail.com>
2023-10-12 12:00:07 +0900

    feat(medium): imp ReplaceFirst

--- questions/25170-medium-replace-first/template.ts
+++ questions/25170-medium-replace-first/template.ts
@@ -1,1 +1,5 @@
-type ReplaceFirst<T extends readonly unknown[], S, R> = any
+type ReplaceFirst<T extends readonly unknown[], S, R> = T extends [infer T1, ...infer Rest]
+  ? T1 extends S
+    ? [R, ...Rest]
+    : [T1, ...ReplaceFirst<Rest, S, R>]
+  : []
