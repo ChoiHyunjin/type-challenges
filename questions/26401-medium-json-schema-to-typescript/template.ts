c2c2e203727bd66b3a2b1a8a246e6a026e09fbcd
choihyunjin <guswls3373@gmail.com>
2023-10-18 10:33:39 +0900

    feat(medium): imp JSONSchema2TS

--- questions/26401-medium-json-schema-to-typescript/template.ts
+++ questions/26401-medium-json-schema-to-typescript/template.ts
@@ -1,1 +1,42 @@
-type JSONSchema2TS<T> = any
+type JSONType = 'string' | 'number' | 'boolean' | 'array' | 'object'
+type JSONMap = {
+  string: string
+  number: number
+  boolean: boolean
+}
+type JSONSchema = {
+  type: JSONType
+  enum?: unknown[]
+  properties?: Record<string, JSONSchema>
+  items?: JSONSchema
+}
+
+type RequiredJSONSchema = JSONSchema & { required?: string[] }
+
+type CombineObjects<T extends object, K extends object> = {
+  [key in keyof (T & K)]: (T & K)[key]
+}
+
+type RequiredTS<T extends object, K extends (keyof T)> =
+      CombineObjects<
+          { [key in keyof T as key extends K ? key : never]-?: T[key] },
+          { [key in keyof T as key extends K ? never : key]: T[key] }>
+
+type JSONSchema2TS<T extends RequiredJSONSchema> =
+    T['type'] extends 'object'
+      ? T['properties'] extends object
+        ? T['required'] extends string[]
+          ? RequiredTS<{ [key in keyof T['properties']]?: JSONSchema2TS<T['properties'][key]> }, T['required'][number]>
+          : { [key in keyof T['properties']]?: JSONSchema2TS<T['properties'][key]> }
+        : Record<string, unknown>
+
+      : T['type'] extends 'array'
+        ? T['items'] extends JSONSchema
+          ? JSONSchema2TS<T['items']>[]
+          : unknown[]
+
+        : T['type'] extends 'string' | 'number' | 'boolean'
+          ? T['enum'] extends unknown[]
+            ? T['enum'][number]
+            : JSONMap[T['type']]
+          : unknown
